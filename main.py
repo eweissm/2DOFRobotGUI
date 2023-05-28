8e98b51263095a3e222d82320fb938416ed5c24e
Ericw <eweissm1@asu.edu>
2023-05-28 10:10:55 -0700

    New Commit

--- main.py
+++ main.py
@@ -21,9 +21,12 @@ def plot(x_coord, y_coord, theta, L1, L2):
     # adding the subplot
     plot1 = fig.add_subplot(111)
 
+    plot1.set_xlim([-(L1+L2), (L1+L2)])
+    plot1.set_ylim([-(L1+L2), (L1+L2)])
+
     # plotting the graph
     plot1.plot(0, 0, marker="o", markersize=20)
-    plot1.plot(x_coord, y_coord, marker="o", markersize=20 )
+    plot1.plot(x_coord, y_coord, marker="o", markersize=10 )
     plot1.plot(L1*np.cos(theta[0]), L1*np.sin(theta[0]), marker="o", markersize=20)
     plot1.plot([0, L1*np.cos(theta[0])], [0, L1*np.sin(theta[0])])
     plot1.plot([L1 * np.cos(theta[0]),x_coord], [L1 * np.sin(theta[0]), y_coord])
